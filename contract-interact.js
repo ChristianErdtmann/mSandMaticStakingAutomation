9a3b15f43ce57f918464e502463639480773a8e1
ChristianErdtmann <96842089+ChristianErdtmann@users.noreply.github.com>
2022-04-09 20:04:09 +0200

    Updated int to string

--- contract-interact.js
+++ contract-interact.js
@@ -7,4 +7,4 @@ const contractJson = fs.readFileSync('./abi.json')
 const abi = JSON.parse(contractJson)
 const mSandMaticContract = new web3.eth.Contract(abi, contractAddress)
 
-mSandMaticContract.balanceOf(0x7e5475290Df8E66234A541483453B5503551C780)
+mSandMaticContract.balanceOf('0x7e5475290Df8E66234A541483453B5503551C780')
